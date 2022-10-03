a8a0618c69c302010b3b97e2537f64b035ee6f93
simplecode01 <putravi01@gmail.com>
2022-10-03 15:05:30 +0700

    first commit

---
+++ src/MEEK.java
@@ -0,0 +1,5 @@
+public class MEEK {
+    public static void main (String[] args){
+        System.out.println("AWOKAWOK");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String babi = "JUMLAH BABI PINK ADA = ";
+        int jumlah = 10;
+        System.out.println(babi + jumlah);
+
+    }
+}
